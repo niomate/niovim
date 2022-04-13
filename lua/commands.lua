7d3fd113c77ecb4aaa63cd958d133b3f802f9434
Daniel Gusenburger <d.gusenburger@zema.de>
2022-04-13 10:52:21 +0200

    doc: added todos

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,6 +1,11 @@
+local CONFIG_PATH = "~/.config/nvim/"
+
 vim.api.nvim_create_user_command('OpenConfig', function(opts)
-	local path = "~/.config/nvim/" .. opts.args
+	-- TODO: Autocompletion with all files in the config directory
+	-- TODO: Extract config path from vim functions
+	local path = CONFIG_PATH .. "lua/" .. opts.args .. ".lua"
 	vim.cmd("e " .. path)
 end, { nargs = 1 })
 
-vim.api.nvim_create_user_command('ReloadConfig', 'source ~/.config/nvim/init.lua', { nargs = 0 })
+-- TODO: Doesn't really work yet
+vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', { nargs = 0 })
