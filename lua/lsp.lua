c1312fe5dfbd33b8b7b2781917e5e699fce72914
Daniel Gusenburger <d.gusenburger@zema.de>
2024-03-25 12:54:47 +0100

    ruff changes

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,13 +1,8 @@
 local nvim_lsp = require("lspconfig")
-local util = require "lspconfig/util"
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- if client.name == "ruff_lsp" then
-  --   -- Disable hover in favor of pyright
-  --   client.server_capabilities.hoverProvider = false
-  -- end
   local opts = {noremap = true, silent = true}
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
@@ -19,6 +14,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches'cssls'
 local servers = {
+	"ruff_lsp",
   "rust_analyzer",
   "bashls",
   "vimls",
@@ -27,43 +23,6 @@ local servers = {
   "lua_ls",
   "html",
   "cssls"
-}
-
--- nvim_lsp.pyright.setup {
---   on_attach = on_attach,
---   flags = {
---     debounce_text_changes = 150
---   },
---   root_dir = function(fname)
---     local root_files = {
---       "pyproject.toml",
---       "setup.py",
---       "setup.cfg",
---       "requirements.txt",
---       "Pipfile",
---       "pyrightconfig.json"
---     }
---     return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
---   end,
---   settings = {
---     pyright = {
---       -- Using Ruff's import organizer
---       disableOrganizeImports = true
---     },
---     python = {
---       analysis = {
---         -- Ignore all files for analysis to exclusively use Ruff for linting
---         ignore = {"*"}
---       }
---     }
---   }
--- }
-
-nvim_lsp.ruff_lsp.setup {
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150
-    },
 }
 
 for _, lsp in ipairs(servers) do
