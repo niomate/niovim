4ef5a9bc23b7e281ae9ca9bccb7fb7ec2675d373
Daniel Gusenburger <d.gusenburger@zema.de>
2024-03-25 12:50:54 +0100

    experiment: trying to disable pyright and exclusively use ruff lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -4,10 +4,10 @@ local util = require "lspconfig/util"
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  if client.name == "ruff_lsp" then
-    -- Disable hover in favor of pyright
-    client.server_capabilities.hoverProvider = false
-  end
+  -- if client.name == "ruff_lsp" then
+  --   -- Disable hover in favor of pyright
+  --   client.server_capabilities.hoverProvider = false
+  -- end
   local opts = {noremap = true, silent = true}
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
@@ -29,52 +29,41 @@ local servers = {
   "cssls"
 }
 
-nvim_lsp.pyright.setup {
-  on_attach = on_attach,
-  flags = {
-    debounce_text_changes = 150
-  },
-  root_dir = function(fname)
-    local root_files = {
-      "pyproject.toml",
-      "setup.py",
-      "setup.cfg",
-      "requirements.txt",
-      "Pipfile",
-      "pyrightconfig.json"
-    }
-    return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
-  end,
-  settings = {
-    pyright = {
-      -- Using Ruff's import organizer
-      disableOrganizeImports = true
-    },
-    python = {
-      analysis = {
-        -- Ignore all files for analysis to exclusively use Ruff for linting
-        ignore = {"*"}
-      }
-    }
-  }
-}
+-- nvim_lsp.pyright.setup {
+--   on_attach = on_attach,
+--   flags = {
+--     debounce_text_changes = 150
+--   },
+--   root_dir = function(fname)
+--     local root_files = {
+--       "pyproject.toml",
+--       "setup.py",
+--       "setup.cfg",
+--       "requirements.txt",
+--       "Pipfile",
+--       "pyrightconfig.json"
+--     }
+--     return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+--   end,
+--   settings = {
+--     pyright = {
+--       -- Using Ruff's import organizer
+--       disableOrganizeImports = true
+--     },
+--     python = {
+--       analysis = {
+--         -- Ignore all files for analysis to exclusively use Ruff for linting
+--         ignore = {"*"}
+--       }
+--     }
+--   }
+-- }
 
 nvim_lsp.ruff_lsp.setup {
     on_attach = on_attach,
     flags = {
       debounce_text_changes = 150
     },
-		root_dir = function(fname)
-			local root_files = {
-				"pyproject.toml",
-				"setup.py",
-				"setup.cfg",
-				"requirements.txt",
-				"Pipfile",
-				"pyrightconfig.json"
-			}
-			return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
-		end,
 }
 
 for _, lsp in ipairs(servers) do
